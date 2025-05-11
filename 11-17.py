b76db975398b3bbb6adbc7c21e31bc05fe7a22e5
uzovka <dev@null>
2025-05-11 09:49:39 +0200

    sssss

---
+++ 11-17.py
@@ -0,0 +1,12 @@
+import turtle as t
+"""11) S využitím želví grafiky napište funkci draw_pictures, ktera bude mít na vstupu zadán počet vrchlů n-úhelníku 
+a počet kopií tohoto n-úhelníku rovnoměrně rozprostřených po kružnici, a která tyto n-úhelníky vykreslí."""
+"""12) S využitím želví grafiky napište program, který vykreslí zadaný počet soustředných kružnic (max 30) se středy 
+v bodě [0,0] = střed plátna a s různými poloměry. Barvy kružnic generujte náhodně."""
+"""13) S využitím želví grafiky napište program, který vykreslí 36 kružnic o zadaném poloměru r (r je maximálně 150). 
+Kružnice budou mít společnčný bod v bodě [0,0] = střed plátna. Barvy kružnic generujte náhodně."""
+
+"""14) Z klávesnice zadejte délky stran a, b obdélníku (přirozená čísla). Napište program, který vykreslí obdélník tak, aby vždy ležel delší stranou naležato a levý dolní roh obdélníku byl v bodě [100, 250]. Obdélník vybarvěte červenou barvou."""
+"""15) Z klávesnice zadejte jeden z názvů zemí: Nizozemí, Německo, Maďarsko. Na monitor vykreslete barvu příslušné země."""
+"""16) Na monitor vykreslete šachovnici. 8*8 políček s velikostí strany 50."""
+"""17) Na monitor vykreslete plástev medu podle vzoru uvedeného na obrázku Vceli_plastev.jpg."""
---
+++ 26-30.py
@@ -0,0 +1,10 @@
+
+"""26) Na vstupu je dán vzor, který budeme hledat v následující řadě čísel (vzor se v řadě může vyskytnout nejvýše jednou).
+ Dále je na vstupu dáno přirozené číslo n (označující počet prvků v následující řadě čísel) a vlastní řada n čísel.
+ Pomocí metody sekvenčního hledání určete, zda se zadaný vzor v řadě vyskytuje, či nikoliv."""
+"""27) Na vstupu je dán vzor, který budeme hledat v následující řadě čísel (vzor se v řadě může vyskytnout nejvýše jednou).
+ Dále je na vstupu dáno přirozené číslo n (označující počet prvků v následující řadě čísel) a vlastní řada n čísel.
+ Pomocí metody binárního hledání (= metodou půlení intervalu) určete, zda se zadaný vzor v řadě vyskytuje, či nikoliv."""
+"""28) Zadanou řadu čísel seřaďte vzestupně metodou Select Sort"""
+"""29) Zadanou řadu čísel seřaďte vzestupně metodou Insert Sort"""
+"""30) Zadanou řadu čísel seřaďte vzestupně metodou Quick Sort"""
